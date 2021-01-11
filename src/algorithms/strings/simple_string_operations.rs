25be17bc0c398bcaa2b8507dcd7814ca0fe959ff
tobi612 <22715034+tobi6112@users.noreply.github.com>
2021-01-11 17:26:25 +0100

    add reverse string

--- src/algorithms/strings/simple_string_operations.rs
+++ src/algorithms/strings/simple_string_operations.rs
@@ -1,4 +1,4 @@
-/// Count the occurence of a character in a given string
+/// Count the occurence of a character in a given string slice
 ///
 /// # Examples
 ///
@@ -9,6 +9,12 @@ fn count_char(s: &str, c: &char) -> usize {
   s.chars().filter(|ch| ch == c).count()
 }
 
+///Reverses a given string slice
+///
+/// # Examples
+/// ```rust
+/// assert_eq!("!dlroW ,olleH", reverse_string("Hello, World!"))
+/// ```
 fn reverse_string(s: &str) -> String {
   s.chars().rev().collect()
 }
@@ -20,5 +26,10 @@ mod test {
   #[test]
   fn count_char_with_banana() {
     assert_eq!(3, count_char("banana", &'a'))
+  }
+
+  #[test]
+  fn reverse_hello_world() {
+    assert_eq!("!dlroW ,olleH", reverse_string("Hello, World!"))
   }
 }
